ae1de26aab2626451d6f404aafb51c372cf436ab
Alexis BEHIER <alexisbehier22@gmail.com>
2017-04-25 12:51:22 +0200

    Rendu Projet

--- Projet/project.cpp
+++ Projet/project.cpp
@@ -2,20 +2,14 @@
 #include <boost/graph/bipartite.hpp>
 #include <boost/graph/graphviz.hpp>
 #include <vector>
-#include <random>
-#include <cassert>
-#include <algorithm>
 #include <iostream>
-#include <stdlib.h>     
-#include <time.h>       
+#include <stdlib.h>           
 #include <list>
-#include <map>
 #include <queue>
 #include <string>
 
 using namespace boost;
 using namespace std;
-//using std::vector;
 
 typedef struct etablissement{
     string nom;
@@ -24,6 +18,7 @@ typedef struct etablissement{
 
 typedef struct eleve{
     string nom;
+    string assigne;
     int moyenne;
     int rang;
     list<string> choix;
@@ -48,7 +43,7 @@ typedef struct point {
     }
 }point;
 
-typedef adjacency_list<vecS, vecS, bidirectionalS, point, my_e_property> Graph;
+typedef adjacency_list<listS, vecS, bidirectionalS, point, my_e_property> Graph;
 
 void createPointAndList(Graph &g){
     vector<point> prefEtabl;
@@ -62,7 +57,6 @@ void createPointAndList(Graph &g){
         while ( getline (myfile,line) ){
             istringstream sstream(line);
             getline(sstream, Vetablissement.Petablissement.nom, ';');
-            //cout << Vetablissement.Petablissement.nom << endl;
             getline(sstream, acc);
             Vetablissement.Petablissement.capacite = stoi(acc);
             add_vertex(Vetablissement, g);
@@ -76,15 +70,14 @@ void createPointAndList(Graph &g){
     if (myfile.is_open()){
         Veleve.actual = 1;
         while ( getline (myfile,line) ){
-            //cout << endl;
             istringstream sstream(line);
             getline(sstream, Veleve.Peleve.nom, ';');
             getline(sstream, acc, ';');
             Veleve.Peleve.moyenne = stoi(acc);
             while(getline(sstream, acc, ';')){
-                //cout << acc << endl;
-                Veleve.Peleve.choix.push_front(acc);
+                Veleve.Peleve.choix.push_back(acc);
             }
+            Veleve.Peleve.assigne = "";
             prefEtabl.emplace_back(Veleve);
             add_vertex(Veleve, g);
             Veleve.Peleve.choix.clear();
@@ -147,7 +140,6 @@ void doTheThing(Graph &g){
      pair_it.first != pair_it.second;
      ++pair_it.first) {
         if(g[*pair_it.first].actual == 0){
-            //g[*pair_it.first].Petablissement.capacite
             vector<graph_traits<Graph>::out_edge_iterator> VecEdge;
 
             for(auto pair_itae = out_edges(*pair_it.first, g);
@@ -158,17 +150,15 @@ void doTheThing(Graph &g){
 
             sort(VecEdge.begin(), VecEdge.end(), ([&g] (const graph_traits<Graph>::out_edge_iterator edge1, const graph_traits<Graph>::out_edge_iterator edge2) -> bool {return (g[*edge1].dist < g[*edge2].dist);}));
 
-            for(int i = 0; i < g[*pair_it.first].Petablissement.capacite; i++){
-                g[*VecEdge[i]].oui = 1;
+            for(unsigned int i = 0; (int)i < g[*pair_it.first].Petablissement.capacite && i < VecEdge.size(); i++){
+                    g[*VecEdge[i]].oui = 1;
             }
         }
     }
-
     for (auto pair_it = vertices(g);
      pair_it.first != pair_it.second;
      ++pair_it.first) {
             if(g[*pair_it.first].actual == 1){
-            //g[*pair_it.first].Petablissement.capacite
             vector<graph_traits<Graph>::out_edge_iterator> VecEdge;
 
             for(auto pair_itae = out_edges(*pair_it.first, g);
@@ -178,70 +168,129 @@ void doTheThing(Graph &g){
             }
 
             sort(VecEdge.begin(), VecEdge.end(), ([&g] (const graph_traits<Graph>::out_edge_iterator edge1, const graph_traits<Graph>::out_edge_iterator edge2) -> bool {return (g[*edge1].dist < g[*edge2].dist);}));
-
-            g[*VecEdge[0]].oui = 1;
+            if(!VecEdge.empty())
+                g[*VecEdge[0]].oui = 1;
         }
     }
 
-    //bool validate = 0;
-    graph_traits<Graph>::vertex_descriptor actualEleve;
 
+    bool ugh;
     for (auto pair_it = vertices(g);
      pair_it.first != pair_it.second;
      ++pair_it.first) {
-         if(g[*pair_it.first].actual == 1){
+        if(g[*pair_it.first].actual == 0){
             for(auto pair_itae = out_edges(*pair_it.first, g);
-                 pair_itae.first != pair_itae.second;
-                 ++pair_itae.first){
-                if(g[*pair_itae.first].oui == 1)
+                 pair_itae.first != pair_itae.second;){
+                auto acc1 = *pair_itae.first;
+                ugh = 1;
+                if(g[*pair_itae.first].oui == 1){
                     signature = g[*pair_itae.first].signature;
                     for(auto pair_itae2 = out_edges(target(*pair_itae.first, g), g);
                          pair_itae2.first != pair_itae2.second;){
-                        if(g[*pair_itae2.first].signature != -signature && g[*pair_itae.first].oui == 0){
-                            auto acc = *pair_itae2.first;
+                        auto acc2 = *pair_itae2.first;
+                        if(g[*pair_itae2.first].signature == -signature && g[*pair_itae2.first].oui == 1){
+                            ugh = 0;
+                            pair_itae.first++;
                             pair_itae2.first++;
-                            remove_edge(acc, g);
+                            g[source(acc2, g)].Peleve.assigne = g[target(acc2, g)].Petablissement.nom;
+                            g[target(acc2, g)].Petablissement.capacite--;
+                            remove_edge(acc1, g);
+                            remove_edge(acc2, g);
                         }else{
                             pair_itae2.first++;
                         }
-
                     }
+                }
+                if (ugh)
+                    pair_itae.first++;
             }
         }
     }
 
+    bool incomprehension;
+    for (auto pair_it = vertices(g);
+     pair_it.first != pair_it.second;
+     ++pair_it.first) {
+        if(g[*pair_it.first].actual == 1){
+            if(g[*pair_it.first].Peleve.assigne != ""){
+                auto pair_itae = out_edges(*pair_it.first, g);
+                while(pair_itae.first != pair_itae.second){
+                    ugh = 1;
+                    auto acc1 = *pair_itae.first;
+                    auto pair_itae2 = out_edges(target(acc1, g), g);
+                    incomprehension = 0;
+                    while(pair_itae2.first != pair_itae2.second){
+                        auto acc2 = *pair_itae2.first;
+                        if(g[*pair_itae2.first].signature == -g[*pair_itae.first].signature && (!incomprehension)){
+                            incomprehension = 1;
+                            ugh = 0;
+                            pair_itae.first++;
+                            pair_itae2.first++;
+                            remove_edge(acc1, g);
+                            remove_edge(acc2, g);
+                        }
+                        pair_itae2.first++;
+                    }
+                    if (ugh)
+                        pair_itae.first++;
+                }
+            } 
+        }
+    }
 
+    for (auto pair_it = vertices(g);
+     pair_it.first != pair_it.second;
+     ++pair_it.first) {
+        if(g[*pair_it.first].actual == 0){
+            if(g[*pair_it.first].Petablissement.capacite == 0){
+                auto pair_itae = out_edges(*pair_it.first, g);
+                while(pair_itae.first != pair_itae.second){
+                    ugh = 1;
+                    auto acc1 = *pair_itae.first;
+                    auto pair_itae2 = out_edges(target(acc1, g), g);
+                    incomprehension = 0;
+                    while(pair_itae2.first != pair_itae2.second){
+                        auto acc2 = *pair_itae2.first;
+                        if(g[*pair_itae2.first].signature == -g[*pair_itae.first].signature && (!incomprehension)){
+                            incomprehension = 1;
+                            ugh = 0;
+                            pair_itae.first++;
+                            pair_itae2.first++;
+                            remove_edge(acc1, g);
+                            remove_edge(acc2, g);
+                        }
+                        pair_itae2.first++;
+                    }
+                    if (ugh)
+                        pair_itae.first++;
+                }
+            } 
+        }
+    }
 }
 
 
-int main(/*int argc, char *argv[]*/){
+int main(){
     Graph g;
     createPointAndList(g);
-
-    // for (auto pair_it = vertices(g);
-    //  pair_it.first != pair_it.second;
-    //  ++pair_it.first) {
-    //     cout << g[*pair_it.first].Peleve.nom << " :" << endl;
-    //     while(!g[*pair_it.first].Peleve.choix.empty()){
-    //         cout << g[*pair_it.first].Peleve.choix.front() << endl;
-    //         g[*pair_it.first].Peleve.choix.pop_front();
-    //     }
-    //     cout << endl;
-    // }
-
-    // for(unsigned int i = 0; i < prefEtabl.size();i++)
-    //     cout << prefEtabl[i].Peleve.nom << endl;
-
-    // for (auto pair_it = vertices(g);
-    //  pair_it.first != pair_it.second;
-    //  ++pair_it.first) {
-    //     cout << g[*pair_it.first].Peleve.nom << " : " << g[*pair_it.first].Peleve.rang << endl;
-    // }
-
+    int sec = 100;
 
     createEdge(g);
     write_graphviz(std::cout, g);
-    doTheThing(g);
-    write_graphviz(std::cout, g);
+    while(sec--){
+        auto pair_it = edges(g);
+        if(distance(pair_it.first,pair_it.second)==0){
+            sec = 0;
+        }else
+            doTheThing(g);
+            //write_graphviz(std::cout, g);
+    }
+    //write_graphviz(std::cout, g);
+    for (auto pair_it = vertices(g);
+     pair_it.first != pair_it.second;
+     ++pair_it.first) {
+        if(g[*pair_it.first].actual)
+            cout << g[*pair_it.first].Peleve.nom << " : " << g[*pair_it.first].Peleve.assigne << endl;
+    }
     return 0;
 }
